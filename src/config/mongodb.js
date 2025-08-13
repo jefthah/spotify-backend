d680d5ed44e9d705841e0c57b9110aceac5f489b
Jefta Supraja <jefta.supraja@gmail.com>
2025-08-13 16:33:07 +0700

    log error connction

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,10 +1,32 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  // Event listener untuk koneksi yang berhasil
   mongoose.connection.on("connected", () => {
-    console.log("connection established");
+    console.log("MongoDB connection established");
   });
-  await mongoose.connect(`${process.env.MONGODB_URL}/spotify`);
+
+  // Event listener untuk error koneksi
+  mongoose.connection.on("error", (err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
+  // Event listener untuk disconnect
+  mongoose.connection.on("disconnected", () => {
+    console.log("MongoDB connection disconnected");
+  });
+
+  try {
+    // Koneksi ke MongoDB Atlas menggunakan URL dari environment variable
+    await mongoose.connect(`${process.env.MONGODB_URL}/spotify`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connection successful");
+  } catch (err) {
+    console.error("Error while connecting to MongoDB:", err);
+    process.exit(1);  // Keluar dari aplikasi jika koneksi gagal
+  }
 };
 
 export default connectDB;
